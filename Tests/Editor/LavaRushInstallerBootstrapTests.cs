23e28b358ae36714f426a0763f04181e2b6b7755
JewooSong <jewoo@actionfit.kr>
2026-07-21 12:06:23 +0900

    com.actionfit.lava-rush.installer 0.1.9

--- Tests/Editor/LavaRushInstallerBootstrapTests.cs
+++ Tests/Editor/LavaRushInstallerBootstrapTests.cs
@@ -50,7 +50,7 @@ namespace ActionFit.LavaRushInstaller.Editor.Tests
 
             Profile profile = JsonUtility.FromJson<Profile>(asset.text);
             Assert.That(profile.bundleId, Is.EqualTo("lava-rush"));
-            Assert.That(profile.bundleVersion, Is.EqualTo("0.1.8"));
+            Assert.That(profile.bundleVersion, Is.EqualTo("0.1.9"));
             Assert.That(profile.bootstrapPackageId, Is.EqualTo(LavaRushInstallerBootstrap.InstallerPackageId));
             Assert.That(profile.packages.Select(package => package.packageId), Is.EquivalentTo(new[]
             {
@@ -93,9 +93,9 @@ namespace ActionFit.LavaRushInstaller.Editor.Tests
                 package.packageId == "com.actionfit.lava-rush.ui").required, Is.True);
             Assert.That(Version(profile, "com.actionfit.content-core"), Is.EqualTo("0.2.3"));
             Assert.That(Version(profile, "com.actionfit.time"), Is.EqualTo("1.0.4"));
-            Assert.That(Version(profile, "com.actionfit.lava-rush"), Is.EqualTo("0.1.7"));
+            Assert.That(Version(profile, "com.actionfit.lava-rush"), Is.EqualTo("0.1.8"));
             Assert.That(Version(profile, "com.actionfit.ui.foundation"), Is.EqualTo("2.0.0"));
-            Assert.That(Version(profile, "com.actionfit.lava-rush.ui"), Is.EqualTo("0.1.11"));
+            Assert.That(Version(profile, "com.actionfit.lava-rush.ui"), Is.EqualTo("0.1.13"));
             Assert.That(Version(profile, "com.coffee.ui-effect"), Is.EqualTo("5.10.8"));
             Assert.That(Version(profile, "com.coffee.ui-particle"), Is.EqualTo("4.12.1"));
             Assert.That(Version(profile, "com.coffee.softmask-for-ugui"), Is.EqualTo("3.5.0"));
